9f14d0389330484fcad99c99827df455f97e41a9
Catherine Martin <cat@Catherines-MacBook-Air.local>
2024-01-11 17:58:50 -0600

    Copying from local

---
+++ cta_tracker/rgb_display.py
@@ -0,0 +1,6 @@
+class RGBDisplay():
+    def __init__(self, json_response):
+        self.json_response = json_response
+        """
+        Class that will take JSON return by CTA API and display it in RGB. 
+        """
---
+++ run.py
@@ -0,0 +1,21 @@
+import datetime
+from cta_tracker.cta_tracker import CTAtracker
+
+time_args={
+    'peak_start':datetime.time(7,0,0),
+    'peak_end':datetime.time(10,0,0),
+    'sleep_start':datetime.time(22,0,0),
+    'sleep_end':datetime.time(2,0,0),
+}
+
+url_args={
+    'stpid':[],
+    'mapid':['40570'],
+    'max_results':8
+}
+
+cta = CTAtracker(time_args, url_args)
+print(cta.peak_start)
+print(cta.url)
+
+cta.api_loop()
