18149b0070b35e296d77140112ceaa981939c2d1
Catherine Martin <cat@Catherines-MacBook-Air.local>
2024-03-13 18:21:59 -0500

    Refactor to loop through list rather than static date/name pair

--- countdown/countdown.py
+++ countdown/countdown.py
@@ -4,31 +4,38 @@ from PIL import Image
 
 class Countdown:
     """
-    date_of_event: String of format 'yyyy-mm-dd'
-    event_display: String to be displayed as '{x} days until {event_display}'
+    image_location: String to path of txt file containing relevent information
     image_location: String of path to display image location
     """
 
     def __init__(self,
-                 date_of_event,
-                 event_display,
+                 list_location,
                  image_location):
-        self.date_of_event = date_of_event
-        self.event_display = event_display
+        self.list_location = list_location
         self.image_location = image_location
         self.image_thumbnail = Countdown.image_getter(self)
 
-    def days_until_getter(self):
-        date_elements = self.date_of_event.split('-')
+    @staticmethod
+    def get_line_date(current_line):
+        current_line_date = current_line.split(',')[0]
+        date_elements = current_line_date.split('-')
         date_elements = [int(x) for x in date_elements]
-        d0 = date(date_elements[0], date_elements[1], date_elements[2])
-        d1 = date.today()
-        days_until = (d0 - d1).days
-        # print(f'{days_until} days until {self.event_display}!')
-        return days_until
+        return date(date_elements[0], date_elements[1], date_elements[2])
+
+    def current_line_getter(self):
+        d_today = date.today()
+        d_line, n_line = date(2024, 1, 1), 'No event'  # dummy date to start loop with
+
+        f = open(self.list_location)
+        while d_line < d_today:
+            current_line = f.readline()
+            d_line = self.get_line_date(current_line)
+            n_line = current_line.split(',')[1]
+        return d_line, n_line
 
     def image_getter(self):
         """Load and convert image"""
         image = Image.open(self.image_location)
         image.thumbnail((64, 32), Image.ANTIALIAS)
         return image.convert('RGB')
+
